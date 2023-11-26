dcd59e92ef053fc876aea7465f499df60d204bca
Franca Mgbogu <franciedigital@gmail.com>
2023-11-26 04:58:30 +0100

    Make the hero page responsive

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,11 +5,11 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="mt-40">
+    <div className="mt-24 p-4 items-center">
       <div className="relative grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8">
         {/* Left column */}
-        <div className="p-8 flex flex-col justify-center z-10">
-          <h1 className="text-7xl font-bold mb-4 whitespace-pre-line leading-tight">
+        <div className="p-8 flex flex-col justify-center z-10 md:max-w-xl">
+          <h1 className="text-4xl md:text-7xl font-bold mb-4 whitespace-pre-line leading-tight">
             Handmade, With an Extra Pinch of{" "}
             <span className="text-red-500"> Love</span>
           </h1>
@@ -28,7 +28,7 @@ const Hero = () => {
           <img
             src="../../images/pizza-hero.png"
             alt="Hero Image"
-            className="absolute top-[-60px] right-0 left-16 object-cover relative w-[60%]  z-20"
+            className="absolute top-[-60px] right-0 left-16 object-cover relative w-full md:w-[60%] z-20"
           />
           <img
             src="../../images/pizza-onion.png"
@@ -38,7 +38,7 @@ const Hero = () => {
           <img
             src="../../images/pizza-small.png"
             alt="Small pizza Image"
-            className="absolute right-32 bottom-0 z-20 w-[30%]"
+            className="absolute right-8 bottom-0 z-20 w-[30%] md:w-[30%]"
           />
           <div className="absolute w-full h-full rounded-bl-full bg-yellow-500 top-[-30px] right-[-30px] left-[50%] "></div>
         </div>
