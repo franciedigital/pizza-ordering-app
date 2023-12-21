9fcccc331d56bb67c25da27fd88800f76c6dad65
Franca Mgbogu <franciedigital@gmail.com>
2023-12-21 13:28:05 +0100

    Compare password with confirmPassword

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -5,69 +5,66 @@ import Footer from "@/components/footer";
 
 const RegisterForm = ({ handleSubmit, handleInputChange, formData }) => {
   return (
-    <div className="flex flex-col items-center justify-start w-full bg-gray-50">
-      <h2 className="text-2xl font-bold mb-4 mt-8">Create an account</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Email:
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-            placeholder="Joe.Doe@mail.com"
-            required
-            className="mt-1 p-2 w-full border rounded-md"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="password"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-            required
-            className="mt-1 p-2 w-full border rounded-md"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="confirmPassword"
-            className="block text-sm font-medium text-gray-600"
-          >
-            Confirm Password:
-          </label>
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            value={formData.confirmPassword}
-            onChange={handleInputChange}
-            required
-            className="mt-1 p-2 w-full border rounded-md"
-          />
-        </div>
-        <button
-          type="submit"
-          className="bg-green-500 text-white px-8 py-2 rounded-md mt-4 block mx-auto"
+    <form onSubmit={handleSubmit}>
+      <div className="mb-4">
+        <label
+          htmlFor="email"
+          className="block text-sm font-medium text-gray-600"
         >
-          Create
-        </button>
-      </form>
-    </div>
+          Email:
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          placeholder="Joe.Doe@mail.com"
+          required
+          className="mt-1 p-2 w-full border rounded-md"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="password"
+          className="block text-sm font-medium text-gray-600"
+        >
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          required
+          className="mt-1 p-2 w-full border rounded-md"
+        />
+      </div>
+      <div className="mb-4">
+        <label
+          htmlFor="confirmPassword"
+          className="block text-sm font-medium text-gray-600"
+        >
+          Confirm Password:
+        </label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleInputChange}
+          required
+          className="mt-1 p-2 w-full border rounded-md"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-green-500 text-white px-8 py-2 rounded-md mt-4 block mx-auto"
+      >
+        Create
+      </button>
+    </form>
   );
 };
 
@@ -87,13 +84,34 @@ const signup = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Submitted: ", {formData});
+    if (formData.password !== formData.confirmPassword) {
+      return;
+    } else {
+      console.log("Submitted: ", { formData });
+    }
+  };
+
+  const MaybeRenderPasswordError = () => {
+    return formData.password !== formData.confirmPassword ? (
+      <div className="text-red-500">
+        <p>Error! Password does not match</p>
+      </div>
+    ) : null;
   };
 
   return (
     <>
       <Navbar />
-      <RegisterForm handleSubmit={handleSubmit} handleInputChange={handleInputChange} formData={formData} />
+      <div className="flex flex-col items-center justify-start w-full bg-gray-50">
+        <h2 className="text-2xl font-bold mb-4 mt-8">Create an account</h2>
+        <RegisterForm
+          handleSubmit={handleSubmit}
+          handleInputChange={handleInputChange}
+          formData={formData}
+        />
+        <MaybeRenderPasswordError />
+      </div>
+
       <Footer />
     </>
   );
